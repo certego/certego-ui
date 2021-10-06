cce7eca6b41afa26f53dc4433ab3d74f3d1c8203
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-10-06 17:58:56 +0530

    chore: improve form components

--- src/components/form/TernaryCheckbox.jsx
+++ src/components/form/TernaryCheckbox.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Button, ButtonGroup } from 'reactstrap'
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, ButtonGroup } from "reactstrap";
 
 /**
  * @type {component}
  * @param props
  */
 function TernaryCheckbox(props) {
-  const { value, onChange, trueLabel, falseLabel, undefLabel, ...rest } = props
+  const { value, onChange, trueLabel, falseLabel, undefLabel, ...rest } = props;
   return (
     <ButtonGroup {...rest}>
       <Button
-        color={value === false ? 'secondary' : 'tertiary'}
-        onClick={() => onChange(false)}
-        outline={value !== false}
-        active={value === false}
-      >
-        {falseLabel}
-      </Button>
-      <Button
-        color={value ? 'secondary' : 'tertiary'}
+        color={value ? "secondary" : "tertiary"}
         onClick={() => onChange(true)}
         outline={!value}
         active={value}
@@ -27,15 +19,23 @@ function TernaryCheckbox(props) {
         {trueLabel}
       </Button>
       <Button
-        color={typeof value === 'undefined' ? 'secondary' : 'tertiary'}
+        color={value === false ? "secondary" : "tertiary"}
+        onClick={() => onChange(false)}
+        outline={value !== false}
+        active={value === false}
+      >
+        {falseLabel}
+      </Button>
+      <Button
+        color={typeof value === "undefined" ? "secondary" : "tertiary"}
         onClick={() => onChange(undefined)}
-        outline={typeof value !== 'undefined'}
-        active={typeof value === 'undefined'}
+        outline={typeof value !== "undefined"}
+        active={typeof value === "undefined"}
       >
         {undefLabel}
       </Button>
     </ButtonGroup>
-  )
+  );
 }
 
 TernaryCheckbox.propTypes = {
@@ -43,13 +43,13 @@ TernaryCheckbox.propTypes = {
   onChange: PropTypes.func.isRequired,
   falseLabel: PropTypes.string,
   trueLabel: PropTypes.string,
-  undefLabel: PropTypes.string
-}
+  undefLabel: PropTypes.string,
+};
 
 TernaryCheckbox.defaultProps = {
-  falseLabel: 'No',
-  trueLabel: 'Yes',
-  undefLabel: 'All'
-}
+  falseLabel: "No",
+  trueLabel: "Yes",
+  undefLabel: "All",
+};
 
-export default React.memo(TernaryCheckbox)
+export default React.memo(TernaryCheckbox);
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,7 +1,7 @@
 export { default as AsyncSelect } from "./AsyncSelect";
 export { default as ButtonSelect } from "./ButtonSelect";
 export { default as InputCheckBox } from "./InputCheckBox";
-export { default as CustomJsonInput } from "./JsonInput";
+export { default as CustomJsonInput } from "./CustomJsonInput";
 export { default as MultiSelectTextInput } from "./MultiSelectTextInput";
 export { default as Select } from "./Select";
 export { default as TernaryCheckbox } from "./TernaryCheckbox";
