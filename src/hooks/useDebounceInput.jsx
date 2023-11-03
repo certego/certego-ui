03aea63848496ff3eccaa3a57d1591d46769c199
Martina Carella <m.carella@certego.net>
2023-11-03 14:40:20 +0100

    added debounce filter and debounce hook (#57)

---
+++ src/hooks/useDebounceInput.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/*
+ * React hook for debounce input. 
+ */
+export default function useDebounceInput(inputValue, delay, setFunction) {
+    React.useEffect(() => {
+      const timer = setTimeout(() => {
+        setFunction(inputValue);
+      }, delay);
+      return () => clearTimeout(timer);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inputValue]);
+}
