a5a7ae8c81d0e488e014057e32d3e55dde3b0f5e
Martina Carella <m.carella@certego.net>
2023-12-01 14:39:45 +0100

    changed DefaultColumnFilter (#71)
    * changed table filters
    
    * remove DebounceColumnFilter export

--- src/hooks/useDebounceInput.jsx
+++ src/hooks/useDebounceInput.jsx
@@ -4,16 +4,11 @@ import React from "react";
  * React hook for debounce input. 
  */
 export default function useDebounceInput(inputValue, delay, setFunction) {
-  const initialized = React.useRef("");
   React.useEffect(() => {
-    if (initialized.current !== inputValue) {
-      initialized.current = inputValue;
-      const timer = setTimeout(() => {
-        setFunction(inputValue);
-      }, delay);
-      return () => clearTimeout(timer);
-    }
-    return null;
+    const timer = setTimeout(() => {
+      setFunction(inputValue);
+    }, delay);
+    return () => clearTimeout(timer);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [inputValue]);
 }
